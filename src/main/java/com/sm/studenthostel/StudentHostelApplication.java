7578a33d78e8da5c7a2324ca67576ce331f5c35b
msalakhedenov <m.salakhedenov@gmail.com>
2023-05-27 17:02:23 +0600

    project setup

---
+++ src/main/java/com/sm/studenthostel/StudentHostelApplication.java
@@ -0,0 +1,13 @@
+package com.sm.studenthostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHostelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/studenthostel/StudentHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.studenthostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
